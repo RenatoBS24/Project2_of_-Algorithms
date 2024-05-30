82c8e3734b7a4508008e1b8c0d8b43f155fbc392
joseM <U22216232@utp.edu.pe>
2024-05-29 19:15:16 -0500

    Add Package Stack by Yllaconza

---
+++ src/Lists/Stack/UndoRedo.java
@@ -0,0 +1,34 @@
+package Lists.Stack;
+
+public class UndoRedo<E> {
+
+    private final MyStack<E> stack1;
+    private final MyStack<E> stack2;
+
+    public UndoRedo() {
+        stack1 = new MyStack<>();
+        stack2 = new MyStack<>();
+    }
+
+    public void add(E item) {
+        stack1.push(item);
+        stack2.clear();
+    }
+
+    public E undo() throws Exception {
+        if (stack1.size() > 1) {
+            stack2.push(stack1.pop());
+            return stack1.get(stack1.size() - 1);
+        } else {
+            return null;
+        }
+    }
+
+    public E getCurrent() {
+        if (stack1.isEmpty()) {
+            return null;
+        } else {
+            return stack1.get(stack1.size() - 1);
+        }
+    }
+}
